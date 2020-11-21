b2844e3d4b573bc79624c03904a40685806c2ff9
a new hope <saalimshoolichiin@gmail.com>
2020-11-21 11:48:06 +0700

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+#variable
+x = 'Hello, Saalim Shoolichiin here!'
+print(x)
+
+#if else
+if x == 'x':
+    print('No')
+elif x =='y':
+    print('No')
+else:
+    print('I have +5 years experience in web app dev with php frameworks.')
+
+print('Now, I have a new passion in python!')
+
+#forloops
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+print(fruits[0])
+
+for x in range(1,5):
+    print(f'the index is {x}')
+
+print('Thank you')
